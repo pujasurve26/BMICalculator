7dcabc468f34443a8759613c48b369f7b7565f32
Puja Kalpesh Surve <pujakalpesh@Pujas-MacBook-Air.local>
2021-11-24 21:38:46 +0530

    Done with finishing

--- BMICalculator/BMICalculator/ViewController.swift
+++ BMICalculator/BMICalculator/ViewController.swift
@@ -11,20 +11,17 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var lblHeightshow: UILabel!
     @IBOutlet weak var lblWeightshow: UILabel!
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
     }
-    
-    
     
     @IBAction func hightsliderMove(_ sender: UISlider) {
         lblHeightshow.text = ("\(String(format: "%.1f", sender.value)) Cm")
     }
     @IBAction func weightslider(_ sender: UISlider){
         lblWeightshow.text = ("\(String(format: "%.1f", sender.value)) Kg")
-        
     }
     
     @IBAction func btnCalculateTapped(_ sender: UIButton) {
