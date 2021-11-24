82691d256fba83d394dd1c7c3405e3e924488494
Puja Kalpesh Surve <pujakalpesh@Pujas-MacBook-Air.local>
2021-11-24 19:36:50 +0530

    Final commit

--- BMICalculator/BMICalculator/ViewController.swift
+++ BMICalculator/BMICalculator/ViewController.swift
@@ -8,12 +8,38 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var lblHeightshow: UILabel!
+    @IBOutlet weak var lblWeightshow: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
-
+    
+    
+    
+    @IBAction func hightsliderMove(_ sender: UISlider) {
+        lblHeightshow.text = ("\(String(format: "%.1f", sender.value)) Cm")
+    }
+    @IBAction func weightslider(_ sender: UISlider){
+        lblWeightshow.text = ("\(String(format: "%.1f", sender.value)) Kg")
+        
+    }
+    
+    @IBAction func btnCalculateTapped(_ sender: UIButton) {
+        
+        let arrHeight = lblHeightshow.text?.components(separatedBy: " ")
+        let arrWeight = lblWeightshow.text?.components(separatedBy: " ")
+        let height = arrHeight?[0] ?? ""
+        let weight = arrWeight?[0] ?? ""
+        
+        let story = UIStoryboard.init(name: "Main", bundle: nil)
+        let vc = story.instantiateViewController(withIdentifier: "ResultViewController") as! ResultViewController
+        vc.height = height
+        vc.weight = weight
+        navigationController?.pushViewController(vc, animated: true)
+    }
 
 }
 
